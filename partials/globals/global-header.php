b368cb286cfe3e0448576f8f8fc77f40fa22fc91
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-09-04 21:41:31 +0200

    Navigation

--- partials/globals/global-header.php
+++ partials/globals/global-header.php
@@ -1,15 +1,17 @@
 <header id="js-global-header" class="l-gutter m-global-header" role="banner">
 
-    <?php $startpage = null;
+    <?php wp_nav_menu(array('container' => 'nav', 'container_class' => 'js-global-nav m-global-nav', 'items_wrap' => '<ul>%3$s</ul>'));
 
-    if(is_front_page() && is_home()){
+    $startpage = null;
+
+    if(is_front_page()){
         $startpage = true;
     }
 
-    echo ($startpage ? '<h1 class="a-global-header__title">' : '<h3 class="a-global-header__title">');
+    echo ($startpage ? '<h1 class="l-pull-left a-global-header__title">' : '<h3 class="a-global-header__title">');
         echo '<a href="'.esc_url( home_url( '/' ) ).'" rel="home">'.get_bloginfo('name').' <span>&middot;</span> '.get_bloginfo('description').'</a>';
-    echo ($startpage ? '</h1>' : '</h3>');
+    echo ($startpage ? '</h1>' : '</h3>'); ?>
 
-    //wp_nav_menu(array('container' => 'nav', 'container_class' => 'm-global__nav', 'items_wrap' => '<ul>%3$s</ul>'));
-    //get_search_form(); ?>
+
+    <button class="js-global-nav-toggler l-pull-right a-global-nav-toggler">Meny</button>
 </header>
