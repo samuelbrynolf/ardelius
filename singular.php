cd5141e18b87bb4ac0268710034b230c82060aeb
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-10-02 09:27:03 +0200

    Format-adjustments for portraits-format, init

--- singular.php
+++ singular.php
@@ -1,10 +1,24 @@
 <?php get_header();
 
-	while ( have_posts() ) { the_post(); ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php get_template_part('partials/singular/singular-header');
+	while ( have_posts() ) { the_post();
+		$featured_format_is_landscape = get_field('acf-featured-img-ratio');
+		//echo ($featured_format_is_landscape ? '' : 'is-not-landscape'); ?>
 
-			echo '<div id="js-textcontent" class="l-container">';
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="m-single__header">
+				<?php the_title( '<h1 class="l-gutter a-title-XL a-single__header-title">', '</h1>' );
+				if(function_exists('makeitSrcset') && has_post_thumbnail()) {
+					makeitSrcset(get_post_thumbnail_id($post->ID), null, null, null, null, null, 'js-scroll_to_content');
+				}
+
+			    if ( is_user_logged_in() ) {
+			        echo '<p class="l-gutter a-fineprint edit-post">';
+			            edit_post_link( 'Redigera inl&auml;gg', ' &mdash; ', '' );
+			        echo '</p>';
+			    } ?>
+			</header>
+
+			<?php echo '<div id="js-textcontent" class="l-container">';
 				echo '<div class="l-span-C9 l-span-D8 m-single__text">';
 					if($content = $post->post_content ) {
 						the_content();
@@ -14,7 +28,20 @@
 				echo '</div>';
 			echo '</div>';
 
-			get_template_part('partials/singular/singular-gallery');
+			if(function_exists('get_sub_field') && have_rows('acf_img-gallery')){
+				echo '<div class="l-container o-gallery js-layout-3" data-columns>';
+				while (have_rows('acf_img-gallery')){
+					the_row();
+					if(get_sub_field('acf_img-gallery_img') && function_exists('makeitSrcset')){
+						makeitSrcset(get_sub_field('acf_img-gallery_img'), null, null, null, null, null, 'm-gallery__item', null, 'true');
+						//makeitSrcset(get_post_thumbnail_id($postID), $vw_mq1, $vw_mq2, $vw_mq3, $vw_mq4, $vw_mq5, null, null, ($lightbox ? 'lightbox-true' : ''));
+						//echo '<h2>'.get_sub_field('acf_img-gallery_img-title').'</h2>';
+						//echo '<p>'.get_sub_field('acf_img-gallery_img-meta').'</p>';
+					}
+				}
+				echo '</div>';
+			}
+
 			get_template_part('partials/singular/singular-aside'); ?>
 		</article>
 	<?php }
