7720ce3deb3312c5e7f86541c2c3046d4c576f22
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-10-04 17:24:15 +0200

    src js

--- singular.php
+++ singular.php
@@ -4,46 +4,45 @@
 		$featured_format_is_landscape = get_field('acf-featured-img-ratio');
 		//echo ($featured_format_is_landscape ? '' : 'is-not-landscape'); ?>
 
-		<article id="post-<?php the_ID(); ?>" class="<?php echo ($featured_format_is_landscape ? '' : 'l-clearfix o-single_article is-not-landscape'); ?>">
-			<header class="m-single__header">
-				<?php the_title( '<h1 class="l-gutter a-title-XL a-single__header-title">', '</h1>' );
-				if(function_exists('makeitSrcset') && has_post_thumbnail()) {
-					makeitSrcset(get_post_thumbnail_id($post->ID), null, null, null, null, null, 'js-scroll_to_content');
+		<article id="post-<?php the_ID(); ?>" class="o-single_article">
+			<section class="l-clearfix o-single_main-content<?php echo ($featured_format_is_landscape ? '' : ' is-not-landscape'); ?>">
+
+				<?php if(function_exists('makeitSrcset') && has_post_thumbnail()) {
+					makeitSrcset(get_post_thumbnail_id($post->ID), null, null, null, null, null, 'm-single__featured-img');
 				}
 
-			    if ( is_user_logged_in() ) {
-			        echo '<p class="l-gutter a-fineprint edit-post">';
-			            edit_post_link( 'Redigera inl&auml;gg', ' &mdash; ', '' );
-			        echo '</p>';
-			    } ?>
-			</header>
+				echo '<div id="js-textcontent" class="m-single__text">';
+					the_title( '<h1 class="a-single__header-title">', '</h1>' );
 
-			<?php echo '<div id="js-textcontent" class="l-container m-single__text">';
-				echo '<div class="l-span-C9 l-span-D8">';
 					if($content = $post->post_content ) {
 						the_content();
 					} elseif(function_exists('get_field') && get_field('acf_text-summary')){
 						echo get_field('acf_text-summary');
 					}
+
+					if ( is_user_logged_in() ) {
+						echo '<p class="a-fineprint edit-post">';
+							edit_post_link( 'Redigera inl&auml;gg', ' &mdash; ', '' );
+						echo '</p>';
+					}
 				echo '</div>';
-			echo '</div>';
+			echo '</section>';
 
 			if(function_exists('get_sub_field') && have_rows('acf_img-gallery')){
 				echo '<div class="l-container o-gallery js-layout-3" data-columns>';
-				while (have_rows('acf_img-gallery')){
-					the_row();
-					if(get_sub_field('acf_img-gallery_img') && function_exists('makeitSrcset')){
-						makeitSrcset(get_sub_field('acf_img-gallery_img'), null, null, null, null, null, 'm-gallery__item', null, 'true');
-						//makeitSrcset(get_post_thumbnail_id($postID), $vw_mq1, $vw_mq2, $vw_mq3, $vw_mq4, $vw_mq5, null, null, ($lightbox ? 'lightbox-true' : ''));
-						//echo '<h2>'.get_sub_field('acf_img-gallery_img-title').'</h2>';
-						//echo '<p>'.get_sub_field('acf_img-gallery_img-meta').'</p>';
+					while (have_rows('acf_img-gallery')){
+						the_row();
+						if(get_sub_field('acf_img-gallery_img') && function_exists('makeitSrcset')){
+							makeitSrcset(get_sub_field('acf_img-gallery_img'), null, null, null, null, null, 'm-gallery__item', null, 'true');
+						}
 					}
-				}
 				echo '</div>';
 			}
 
+		echo '</article>';
+
 			get_template_part('partials/singular/singular-aside'); ?>
-		</article>
+
 	<?php }
 
 get_footer();
