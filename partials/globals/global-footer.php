42b177678042ff05ce84638d64657d19d67a7198
Samuel Brynolf <samuel.brynolf@aventyret.com>
2016-10-18 09:03:05 +0200

    Styling alpha/beta header and alpha/beta footer

--- partials/globals/global-footer.php
+++ partials/globals/global-footer.php
@@ -5,9 +5,9 @@
                 makeitSrcset(get_field('acf_avatar', 'option'), 100, 100, 100, 100, 100, 'a-avatar');
             } ?>
         </div>
-        <div class="l-span-A12 l-pre-B1 l-pre-C0 l-span-B7 l-span-C8 l-span-D7 m-bio">
+        <div class="l-span-A12 l-pre-B0 l-pre-C0 l-span-B7 l-span-C8 l-span-D7 m-bio">
             <?php if(function_exists('get_field') && get_field('acf_biografi_title', 'option')) {
-                echo '<h3 class="a-bio-title a-title-S">';
+                echo '<h3 class="a-bio-title">';
                     the_field('acf_biografi_title', 'option');
                 echo '</h3>';
             }
@@ -15,8 +15,11 @@
             if(function_exists('get_field') && get_field('acf_biografi', 'option')) {
                 echo '<div class="m-bio-text">';
                     the_field('acf_biografi', 'option');
-                    echo '<p>&copy; All Rights Reserved '.date("Y").'</p>';
+
                 echo '</div>';
+
+                echo '<p class="a-fineprint a-global-footer_credentials">&copy; All Rights Reserved '.date("Y").'</p>';
+
             } ?>
         </div>
     </div>
